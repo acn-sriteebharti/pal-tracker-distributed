1bb574381f29f7e8fac72bd81008d657470a1731
K Flay <kasandara.sullivan@accenture.com>
2018-11-05 10:18:35 -0500

    update rest support, up to Update RestOperations.3

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/SpringDefaultController.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/SpringDefaultController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SpringDefaultController {
+public class  SpringDefaultController {
 
     @GetMapping("/")
     public String defaultRoute() {
